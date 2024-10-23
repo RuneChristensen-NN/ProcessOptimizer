0199c56274e8dd94c557ea6bf83f8a40c76cdc12
RUCT <ruct@novonordisk.com>
2024-10-23 15:12:58 +0200

    example amnd classical designs WIP

---
+++ ProcessOptimizer/doe/classical_designs.py
@@ -0,0 +1,13 @@
+import math
+import warnings
+
+import numpy as np
+import patsy
+
+from .doe_transform import doe_to_real_space
+
+# Defining a number of helper functions for the optimal design of experiments
+
+
+# In here goes some classical designs.
+# They could be adapted from https://github.com/tisimst/pyDOE
